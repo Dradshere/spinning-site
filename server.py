ec787effc3a7ad982b0079b1a35181f7ccd71f91
moos4 <moos.kaas.wat@gmail.com>
2025-10-13 17:03:24 +0200

    fixed login for the python server and edited server.py

--- server.py
+++ server.py
@@ -1,4 +1,22 @@
-def main():
-    print("werkt dit")
+from flask import Flask, render_template, request, redirect, url_for
 
-main()
+app = Flask(__name__)
+
+USERNAME = "admin"
+PASSWORD = "password"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == USERNAME and password == PASSWORD:
+            return redirect(url_for("Welcome"))
+        else:
+            return render_template("login.html",
+                                   error="Invalid usernae or password")
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
